6b8c974343001899417a41a56d22c9dc42e3fff0
Bence <herincs.bence.kdg@gmail.com>
2023-11-15 14:45:03 +0100

    Touch up

--- src/main/java/com/example/Sorsolo/MainController.java
+++ src/main/java/com/example/Sorsolo/MainController.java
@@ -1,6 +1,5 @@
 package com.example.Sorsolo;
 
-import com.sun.tools.javac.Main;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
